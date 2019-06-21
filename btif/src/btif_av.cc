7bb53414c08ea112763f48df006cf34ed9fe7d7f
Satheesh Kumar Pallemoni <spallemo@codeaurora.org>
2019-06-21 14:48:00 +0530

    A2dpSrc: Handle av_coll_detected_timer on index basis.
    - Decrease the timeout for AVDT_SIG_GET_ALLCAP from 15s to 4s like
      AVDT_SIG_GETCAP.
    
    - av_coll_detected_timer has been implemented on index basis.
    
    - When collision happened due to some reason during signalling, and
      AV connection got failed, we would trigger 4 sec collision timer,
      and retry for the connection. If it got succeeded conn_retry_count
      varibale need to reset for 1, so that for the next time onwards also
      it would trigger connection for one more time whenever collision
      happens.
    
    CRs-Fixed: 2477120
    Change-Id: I2187a8fb1e3de394487f69137a5fee74dded2ba3

--- btif/src/btif_av.cc
+++ btif/src/btif_av.cc
@@ -201,29 +201,19 @@ typedef struct {
   btav_a2dp_codec_config_t codec_config;
 } btif_av_codec_config_req_t;
 
+typedef struct {
+  RawAddress bd_addr;
+  uint8_t conn_retry_count;
+  alarm_t *av_coll_detected_timer;
+} btif_av_collision_detect_t;
+
 /*****************************************************************************
  *  Static variables
  *****************************************************************************/
 static btav_source_callbacks_t* bt_av_src_callbacks = NULL;
 static btav_sink_callbacks_t* bt_av_sink_callbacks = NULL;
-static btif_av_cb_t btif_av_cb[BTIF_AV_NUM_CB] = {
-    { 0, {{0}}, false, 0, 0, 0, 0, std::vector<btav_a2dp_codec_config_t>(), false,
-    false, false, BTIF_AV_STATE_IDLE, BTA_A2DP_SOURCE_SERVICE_ID,
-    false, false, false, 0, false, false
-#if (TWS_ENABLED == TRUE)
-    , false, false
-#endif
-    , false
-    , 0, 0x1000, 0, {0}, NULL, NULL, false,},
-    { 0, {{0}}, false, 0, 0, 0, 0, std::vector<btav_a2dp_codec_config_t>(), false,
-    false, false, BTIF_AV_STATE_IDLE, BTA_A2DP_SOURCE_SERVICE_ID,
-    false, false, false, 0, false, false
-#if (TWS_ENABLED == TRUE)
-    , false, false
-#endif
-    , false
-    , 0, 0x1000, 0, {0}, NULL, NULL, false,},
-};
+static btif_av_cb_t btif_av_cb[BTIF_AV_NUM_CB];
+btif_av_collision_detect_t collision_detect[BTIF_AV_NUM_CB];
 
 static alarm_t* av_open_on_rc_timer = NULL;
 static btif_sm_event_t idle_rc_event;
@@ -232,10 +222,7 @@ int btif_max_av_clients = 1;
 static bool enable_multicast = false;
 static bool is_multicast_supported = false;
 static bool multicast_disabled = false;
-static RawAddress retry_bda;
 static RawAddress codec_bda = {};
-static int conn_retry_count = 1;
-static alarm_t *av_coll_detected_timer = NULL;
 static bool isA2dpSink = false;
 static bool codec_config_update_enabled = false;
 bool is_codec_config_dump = false;
@@ -661,34 +648,53 @@ static void btif_report_source_codec_state(UNUSED_ATTR void* p_data,
 }
 
 
-static void btif_av_collission_timer_timeout(UNUSED_ATTR void *data) {
-  RawAddress *target_bda = &retry_bda;
+static void btif_av_collission_timer_timeout(void *data) {
+  int *arg = (int *)data;
+  if (!arg) {
+    BTIF_TRACE_ERROR("%s: index is null, return", __func__);
+    return;
+  }
+
+  int collision_index = *arg;
+  if ((collision_index < 0) && (collision_index >= btif_max_av_clients)) {
+    BTIF_TRACE_ERROR("%s: collision index is not valid, return", __func__);
+    return;
+  }
+
+  RawAddress target_bda = collision_detect[collision_index].bd_addr;
   btif_sm_state_t av_state;
   RawAddress av_address;
+  BTIF_TRACE_DEBUG("%s: collision_index = %d target_bda: %s, conn_retry_count = %d", __func__,
+                      collision_index, target_bda.ToString().c_str(),
+                      collision_detect[collision_index].conn_retry_count);
 
-  if(btif_storage_is_device_bonded(target_bda) != BT_STATUS_SUCCESS){
+  if (btif_storage_is_device_bonded(&target_bda) != BT_STATUS_SUCCESS){
     BTIF_TRACE_IMP("btif_av_collission_timer_timeout: not bonded device ");
+    if (arg) {
+      osi_free(arg);
+    }
     return;
-  }else{
+  } else {
     BTIF_TRACE_IMP("btif_av_collission_timer_timeout: bonded device ");
   }
 
-  av_address = *target_bda;
+  av_address = target_bda;
   av_state = btif_get_conn_state_of_device(av_address);
-  BTIF_TRACE_DEBUG("%s(): AV state: %d", __func__, av_state);
-  BTIF_TRACE_DEBUG("TARGET BD ADDRESS %s", av_address.ToString().c_str());
+  BTIF_TRACE_DEBUG("%s: AV state: %d BD_Addr: %s", __func__,
+                      av_state, av_address.ToString().c_str());
 
-  if (av_state == BTIF_AV_STATE_IDLE && conn_retry_count <= 1) {
+  if (av_state == BTIF_AV_STATE_IDLE &&
+      collision_detect[collision_index].conn_retry_count <= 1) {
     if (bt_av_src_callbacks != NULL) {
       BTIF_TRACE_DEBUG("%s Starting A2dp connection", __FUNCTION__);
-      conn_retry_count++;
-      btif_queue_connect(UUID_SERVCLASS_AUDIO_SOURCE, *target_bda, connect_int,
+      collision_detect[collision_index].conn_retry_count++;
+      btif_queue_connect(UUID_SERVCLASS_AUDIO_SOURCE, target_bda, connect_int,
                     btif_max_av_clients);
     } else {
       BTIF_TRACE_DEBUG("%s Aborting A2dp connection retry", __FUNCTION__);
     }
-  } else if (btif_rc_get_connected_peer_handle(av_address) == BTRC_HANDLE_NONE
-            && conn_retry_count <= 1) {
+  } else if (btif_rc_get_connected_peer_handle(av_address) == BTRC_HANDLE_NONE &&
+             collision_detect[collision_index].conn_retry_count <= 1) {
     tBTA_AV_HNDL handle;
     int idx = btif_av_idx_by_bdaddr(&av_address);
     if (idx == btif_max_av_clients) {
@@ -700,36 +706,83 @@ static void btif_av_collission_timer_timeout(UNUSED_ATTR void *data) {
     BTIF_TRACE_DEBUG("%s Starting Avrcp connection for handle: %d", __FUNCTION__, handle);
     if ((handle != (tBTA_AV_HNDL)INVALID_INDEX) && (bt_av_src_callbacks != NULL)) {
       BTA_AvOpenRc(handle);
-      conn_retry_count++;
+      collision_detect[collision_index].conn_retry_count++;
     } else {
       BTIF_TRACE_DEBUG("%s Aborting Avrcp connection retry", __FUNCTION__);
     }
   } else {
-    if (conn_retry_count > 1) {
-      conn_retry_count = 1;
-      BTIF_TRACE_DEBUG("%s Connection Retry count exceeded", __FUNCTION__);
+    if (collision_detect[collision_index].conn_retry_count > 1) {
+      collision_detect[collision_index].conn_retry_count = 1;
+      BTIF_TRACE_DEBUG("%s Connection Retry count exceeded for : %s",
+                       __FUNCTION__, collision_detect[collision_index].bd_addr.ToString().c_str());
+      memset(&collision_detect[collision_index].bd_addr, 0, sizeof(RawAddress));
+      if (arg) {
+        osi_free(arg);
+      }
       return;
     }
     BTIF_TRACE_DEBUG("%s A2dp already connected", __FUNCTION__);
     BTIF_TRACE_DEBUG("%s Avrcp already connected on handle: %d", __FUNCTION__,
                    btif_rc_get_connected_peer_handle(av_address));
   }
+
+  if (arg) {
+    osi_free(arg);
+  }
 }
 
 static void btif_av_check_and_start_collission_timer(int index) {
-  retry_bda = btif_av_cb[index].peer_bda;
+  int coll_i = 0;
+  int *arg = NULL;
+  arg = (int *) osi_malloc(sizeof(int));
+  RawAddress target_bda = btif_av_get_addr_by_index(index);
+  BTIF_TRACE_DEBUG("%s: index: %d ", __func__, index);
 
-  BTIF_TRACE_DEBUG("%s(), index: %d ", __func__, index);
-
-  if (alarm_is_scheduled(av_coll_detected_timer)) {
-    alarm_cancel(av_coll_detected_timer);
-    BTIF_TRACE_DEBUG("btif_av_check_and_start_collission_timer:Deleting previously queued timer");
+  //check for free index, to start timer
+  for (coll_i = 0; coll_i < btif_max_av_clients; coll_i++) {
+    if (alarm_is_scheduled(collision_detect[coll_i].av_coll_detected_timer)) {
+      BTIF_TRACE_DEBUG("%s: collision alram is already scheduled on coll_i = %d,"
+                       " coll_add: %s", __func__, coll_i,
+                       collision_detect[coll_i].bd_addr.ToString().c_str());
+      //When 2nd time collision timer started for target_bda as part of
+      //conn_retry_count, in the middile of timer processconnectotherprofiles has
+      //been triggered and connection has been initiated for the same bda.
+      //If it fails, then once again timer has been going to start on the same
+      //target_bda, by checking on the freed collision index. So, already due to
+      //2nd time collision, timer already running on the same target_bda, it is going
+      //to allocate one more collision index for the same bda and start the other
+      //collision timer. So effectively 2 timers will run for the same bda with
+      //different collision indices. So to avoid this, below check is needed.
+      if (collision_detect[coll_i].bd_addr == target_bda) {
+        BTIF_TRACE_DEBUG("%s: collision alram is already scheduled on target_bda = %s,"
+                             "no need to fetch new collision index to start timer.",
+                          __func__, target_bda.ToString().c_str());
+        if (arg) {
+          osi_free(arg);
+        }
+        break;
+      }
+      continue;
+    } else {
+      collision_detect[coll_i].bd_addr = target_bda;
+      *arg = coll_i;
+      /* Start collision detected timeout */
+      BTIF_TRACE_DEBUG("%s: schedule collision alram on coll_i = %d, bd_add: %s",
+                __func__, coll_i, collision_detect[coll_i].bd_addr.ToString().c_str());
+      alarm_set_on_mloop(collision_detect[coll_i].av_coll_detected_timer,
+                          BTIF_TIMEOUT_AV_COLL_DETECTED_MS_2,
+                          btif_av_collission_timer_timeout,
+                          (void *)arg);
+      break;
+    }
   }
-  /* Start collision detected timeout */
-  alarm_set_on_mloop(av_coll_detected_timer,
-             BTIF_TIMEOUT_AV_COLL_DETECTED_MS_2,
-             btif_av_collission_timer_timeout,
-             NULL);
+  if (coll_i >= btif_max_av_clients) {
+    if (arg) {
+      osi_free(arg);
+    }
+    BTIF_TRACE_DEBUG("%s: All collision indices are full, no coll_i has been allocated, "
+                      "for target_bda: %s",__func__, target_bda.ToString().c_str());
+  }
 }
 
 static void btif_av_cache_src_codec_config(btif_sm_event_t event, void* p_data, int index) {
@@ -1139,6 +1192,20 @@ static bool btif_av_state_opening_handler(btif_sm_event_t event, void* p_data,
       if (p_bta_data->open.status == BTA_AV_SUCCESS) {
         state = BTAV_CONNECTION_STATE_CONNECTED;
         av_state = BTIF_AV_STATE_OPENED;
+        RawAddress target_bda = btif_av_get_addr_by_index(index);
+        for (int i = 0; i < btif_max_av_clients; i++) {
+          if (target_bda == collision_detect[i].bd_addr) {
+            BTIF_TRACE_DEBUG("%s: Collision and Target add are matched.", __func__);
+            collision_detect[i].conn_retry_count = 1;
+            if (alarm_is_scheduled(collision_detect[i].av_coll_detected_timer)) {
+               alarm_cancel(collision_detect[i].av_coll_detected_timer);
+               BTIF_TRACE_DEBUG("%s: clear av_coll_detected_timer on i = %d and add = %s",
+                       __func__, i, collision_detect[i].bd_addr.ToString().c_str());
+            }
+            memset(&collision_detect[i].bd_addr, 0, sizeof(RawAddress));
+            break;
+          }
+        }
 /* SPLITA2DP */
         if (btif_a2dp_audio_if_init != true) {
           if (btif_av_is_split_a2dp_enabled()) {
@@ -3677,8 +3744,6 @@ bt_status_t btif_av_init(int service_id) {
   if (btif_av_cb[0].sm_handle == NULL) {
     alarm_free(av_open_on_rc_timer);
     av_open_on_rc_timer = alarm_new("btif_av.av_open_on_rc_timer");
-    alarm_free(av_coll_detected_timer);
-    av_coll_detected_timer = alarm_new("btif_av.av_coll_detected_timer");
     alarm_free(config_alarm);
     config_alarm = alarm_new("btif.a2dp_sink_set_config");
     alarm_free(sink_config_alarm);
@@ -3710,6 +3775,11 @@ bt_status_t btif_av_init(int service_id) {
         BTIF_AV_STATE_IDLE, i);
       btif_av_cb[i].suspend_rsp_track_timer =
                        alarm_new("btif_av.suspend_rsp_track_timer");
+      alarm_free(collision_detect[i].av_coll_detected_timer);
+      collision_detect[i].av_coll_detected_timer =
+                         alarm_new("btif_av.av_coll_detected_timer");
+      memset(&collision_detect[i].bd_addr, 0, sizeof(RawAddress));
+      collision_detect[i].conn_retry_count = 1;
     }
 
     btif_transfer_context(btif_av_handle_event, BTIF_AV_INIT_REQ_EVT,
@@ -3789,6 +3859,19 @@ static bt_status_t init_src( // gghai
     max_connected_audio_devices = BTIF_AV_NUM_CB;
   }
 
+  for (int i = 0; i < max_connected_audio_devices; i++) {
+    memset(&btif_av_cb[i], 0, sizeof(btif_av_cb[i]));
+    btif_av_cb[i].codec_priorities = codec_priorities;
+    btif_av_cb[i].state = BTIF_AV_STATE_IDLE;
+    btif_av_cb[i].service = BTA_A2DP_SOURCE_SERVICE_ID;
+    btif_av_cb[i].aptx_mode = 0X1000;
+    btif_av_cb[i].remote_start_alarm = NULL;
+    btif_av_cb[i].suspend_rsp_track_timer = NULL;
+    btif_av_cb[i].fake_suspend_rsp = false;
+    memset(&collision_detect[i].bd_addr, 0, sizeof(RawAddress));
+    collision_detect[i].conn_retry_count = 1;
+    collision_detect[i].av_coll_detected_timer = NULL;
+  }
   return init_src(callbacks, codec_priorities, max_connected_audio_devices, a2dp_multicast_state);
 }
 
@@ -4211,6 +4294,10 @@ static void cleanup(int service_uuid) {
 
   for (int i = 0; i < btif_max_av_clients; i++) {
     btif_av_clear_suspend_rsp_track_timer(i);
+    collision_detect[i].conn_retry_count = 1;
+    alarm_free(collision_detect[i].av_coll_detected_timer);
+    collision_detect[i].av_coll_detected_timer = NULL;
+    memset(&collision_detect[i].bd_addr, 0, sizeof(RawAddress));
   }
 
   alarm_free(av_open_on_rc_timer);
